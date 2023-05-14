db6c557f9368d0bb2eeb3ce770bdb0adbb2874b4
Joako-Herrero <joaquinherrero13@gmail.com>
2023-05-14 16:53:02 -0300

    añadiendo todas las modificaciones, por alguna razon no se ve el header

---
+++ Front End/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontend-mjh',
+    appId: '1:808925063504:web:d275e3a217ca3565bf2455',
+    storageBucket: 'frontend-mjh.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDi8If-0eg-7wMlvgCkJEPoiGANCwz_4cU',
+    authDomain: 'frontend-mjh.firebaseapp.com',
+    messagingSenderId: '808925063504',
+  },};
